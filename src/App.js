c77b01c6df22068b6833d3a785e5be1e82333686
Sneha Sharma <87299768+SynonymOfLove@users.noreply.github.com>
2023-03-30 22:35:54 +0530

    feat: palindrome website created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [result, setResult] = useState("");
+
+  function handleInputChange(event) {
+    setInputValue(event.target.value);
+  }
+
+  function checkPalindrome() {
+    if (inputValue === "") {
+      setResult("Please enter something");
+    } else if (inputValue === inputValue.split("").reverse().join("")) {
+      setResult(`${inputValue} is a palindrome`);
+    } else {
+      setResult(`${inputValue} is not a palindrome`);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="form_container">
+      <label htmlFor="input">Enter any text or number:</label>
+      <br />
+      <input
+        type="text"
+        id="input"
+        name="input"
+        value={inputValue}
+        onChange={handleInputChange}
+        required
+      />
+      <br />
+      <button type="button" onClick={checkPalindrome}>
+        Check
+      </button>
+      <div className="result">{result}</div>
+    </form>
   );
 }
 
